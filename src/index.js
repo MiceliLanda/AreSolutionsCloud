8be7e76262cc4495e25669a5e03a33153a5b9e8a
MiceliLanda <183381@ids.upchiapas.edu.mx>
2020-11-26 18:46:53 -0600

    fixedUpload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 //configuraciones
 app.set('port',3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 //middleware
 app.use(express.json());
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,25 +1,20 @@
 const { Router } = require('express'), express = require('express'), app = express();
 const router = Router();
+const e = require('express');
 const multer = require('multer');
 var path = require('path');
+const fs = require('fs');
 
-app.set('view_engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));
-
-app.use(multer({
-    dest: path.join(__dirname,'../public/uploads')
-}).single('subida'));
-
-
-//Tengo error de que no lee los archivos EJS, ya esta configurado el motor de plantillas y ni asi.
-router.get('/', (req, res) => {
+router.get('/', (req, res) => { 
     res.render('viewSubir');
-    //res.send('hola');
 });
 
-router.post('/subirArchivo', (req, res) => {
-    console.log(req.file);
-    res.send('Archive upload succesfully!');
+router.post('/subirArchivo', multer({
+    dest: path.join(__dirname, '../public/uploads'),
+}).single('subida'), (req, res, next) => {
+    fs.rename(req.file.path, `./src/public/uploads/${req.file.originalname}`, () => {
+        res.send('Archivo cargado correctamente!');
+    });
 });
 
 module.exports = router, app;
