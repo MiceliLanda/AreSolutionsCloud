b95d4ddf545bc113254d6cf8b479c3e6321e0e1f
MiceliLanda <183381@ids.upchiapas.edu.mx>
2020-11-30 21:10:54 -0600

    ignore

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,7 +5,6 @@ var path = require('path');
 const fs = require('fs');
 const Datos = require('../models/test')
 
-
 router.get('/', (req, res) => { 
     res.render('viewSubir');
 });
@@ -14,15 +13,14 @@ router.post('/subirArchivo', multer({ dest: path.join(__dirname, '../public/uplo
         
         fs.rename(req.file.path, `./src/public/uploads/${req.file.originalname}`, () => {
 
-        let datos = new Datos()
+       /*  let datos = new Datos()
         datos.nombre = req.file.originalname;
-
         datos.save((err, subirarchivo) => {
             if(err) {
                 res.status(500).send(`Error al guardar los datos: ${err}`)
             }
             res.status(200).send({datos: subirarchivo})
-        });
+        }); */
     });
 
 });
